c1a390b6ea561e788b94cd8ac9559882b6f82120
Viet Hoang <ngviethoang0212@gmail.com>
2022-06-24 20:31:05 +0700

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Intro from '../components/intro';
 import Layout from '../components/layout';
 import { getAllPosts } from '../lib/api';
 import Head from 'next/head';
-import { CMS_NAME } from '../lib/constants';
+import { TITLE } from '../lib/constants';
 import Post from '../types/post';
 
 type Props = {
@@ -19,7 +19,7 @@ const Index = ({ allPosts }: Props) => {
     <>
       <Layout>
         <Head>
-          <title>A Random Blog</title>
+          <title>{TITLE}</title>
         </Head>
         <Container>
           <Intro />
